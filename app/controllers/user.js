8b627086b8a73046c696ce399d6f4b4bac6d4f5d
Wy <wy@mail.com>
2024-03-19 16:53:13 +0100

    Add password hash

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,9 @@
 const { User } = require("../models");
+const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
   try {
+    req.body.password= await bcrypt.hash(req.body.password, 10);
     const user = await User.create(req.body);
     res.status(201).json(user);
   } catch (error) {
