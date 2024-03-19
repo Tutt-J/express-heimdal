03b3aa06ed36f5de9d4eac628ff81332346008ab
Wy <wy@mail.com>
2024-03-19 15:07:00 +0100

    correct db config with env

---
+++ app/config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config({ path: "../.env" });
+
+module.exports = {
+  development: {
+    username: process.env.DB_username,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
+    host: process.env.DB_host,
+    dialect: process.env.DB_dialect,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "node_heimdal_test",
+    host: "127.0.0.1",
+    dialect: "mariadb",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "node_heimdal_production",
+    host: "127.0.0.1",
+    dialect: "mariadb",
+  },
+};
--- app/models/wood.js
+++ app/models/wood.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.ENUM,
       values: ["tender", "medium-hard", "hard"],
     },
-  },, {
+  }, {
     sequelize,
     modelName: 'Wood',
   });
