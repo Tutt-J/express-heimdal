8e5edf30c7408c8f91f395ee84a8f034890f223e
Wy <wy@mail.com>
2024-03-19 15:32:17 +0100

    create basic routes

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const db = require("./app/models/index.js");
+const router = require("./app/routes/index.js");
+
+db.sequelize
+.authenticate()
+.then(() => console.log("Database connected ..."))
+.catch((err) => console.log(err));
 
 const app = express()
+app.use("/api", router);
 
 module.exports = app;
