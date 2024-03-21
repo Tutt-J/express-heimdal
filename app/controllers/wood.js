ed3b84a6b39ac2de6ed72e3f0ed26e29edbc3e2f
Wy <wy@mail.com>
2024-03-21 11:51:20 +0100

    update wood

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,4 +1,5 @@
 const { Wood } = require("../models");
+const fs = require('fs');
 
 exports.readAll = async (req, res) => {
   try {
@@ -22,25 +23,73 @@ exports.readByHardness = async (req, res) => {
     res.status(200).json(woods);
   } catch (error) {
     res.status(500).json({
-      message: error.message || `Some error occurred while reading woods with hardness ${hardness}.`,
+      message:
+        error.message ||
+        `Some error occurred while reading woods with hardness ${hardness}.`,
     });
   }
 };
 
 exports.create = async (req, res) => {
   try {
-    const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    const pathname = `${req.protocol}://${req.get("host")}/uploads/${
+      req.file.filename
+    }`;
     const wood = await Wood.create({
       ...JSON.parse(req.body.datas),
-      image: pathname
+      image: pathname,
     });
 
-    console.log(wood);
-    
     res.status(201).json(wood);
   } catch (error) {
     res.status(500).json({
       message: error.message || "Some error occurred while creating new wood.",
     });
   }
-}
+};
+
+exports.update = async (req, res) => {
+  try {
+    //1. Récuperer l'essence de bois
+    const wood = await Wood.findByPk(req.params.id);
+
+    //2. Vérifier si elle existe
+    if (!wood) {
+      return res.status(404).json({
+        error: "Wood not found",
+      });
+    }
+
+    //3. Parser les datas pour les récupérer
+    let newWood = {
+      ...JSON.parse(req.body.datas),
+    };
+
+    if (req.file) {
+      const pathname = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+      }`;
+
+      newWood = {
+        ...newWood,
+        image: pathname,
+      };
+
+      if (wood.image) {
+        const filename = wood.image.split("/uploads/")[1];
+        fs.unlink(`uploads/${filename}`, () => {
+          console.log(`Image ${filename} deleted`);
+        });
+      }
+    }
+
+    //4. Mettre à jour la donnée
+    await wood.update(newWood);
+
+    res.status(200).json(wood);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "Some error occurred while updating new wood.",
+    });
+  }
+};
