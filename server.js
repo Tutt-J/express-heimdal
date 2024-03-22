cc128c5e877465eb6bb79188237035ce665260ff
Wy <wy@mail.com>
2024-03-22 16:15:45 +0100

    reset cors

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ var cors = require("cors");
 
 const app = require("./app.js");
 
-var corsOptions = {
-  origin: process.env.ORIGIN,
-  // This means that if the preflight CORS request succeeds, the response status will be 200
-  optionsSuccessStatus: 200,
-};
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-app.use(cors(corsOptions));
+// var corsOptions = {
+//   origin: process.env.ORIGIN,
+//   // This means that if the preflight CORS request succeeds, the response status will be 200
+//   optionsSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
 
 const port = process.env.PORT
 
